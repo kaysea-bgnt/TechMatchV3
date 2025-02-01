cb230c8a42c907c474ea1099db8079ec66e101d3
kaysea-bgnt <bognotcassandra@gmail.com>
2025-02-01 14:47:19 +0800

    final filter issues

--- techmatch/src/main/java/appdev/com/techmatch/repository/EventRepository.java
+++ techmatch/src/main/java/appdev/com/techmatch/repository/EventRepository.java
@@ -12,26 +12,20 @@ import java.time.LocalDate;
 @Repository
 public interface EventRepository extends JpaRepository<Event, String> {
     List<Event> findByEventType(String type);
-
     List<Event> findByTopics_Name(String topic);
-
-    @Query("SELECT e FROM Event e WHERE (:date IS NULL OR e.startDate <= :date AND e.endDate >= :date) AND (:type IS NULL OR e.eventType = :type)")
-    List<Event> findByDateAndType(@Param("date") LocalDate date, @Param("type") String type);
-
     List<Event> findByTopics_NameIn(List<String> topics);
+    List<Event> findByStartDate(LocalDate date);
+    List<Event> findByUserUserID(String userID);
 
    @Query("SELECT e FROM Event e JOIN e.topics t WHERE t.name IN :topics AND e.eventType = :eventType")
     List<Event> findByTopicsNameInAndEventType(@Param("topics") List<String> topics, @Param("eventType") String eventType);
-
-
-    List<Event> findByStartDate(LocalDate date);
 
     @Query("SELECT new appdev.com.techmatch.dto.EventAttendeeDTO(e.eventID, e.eventName, u.userID, u.username, u.email) " +
     "FROM Event e JOIN e.attendees u " +
     "WHERE e.eventID = :eventID")
     List<EventAttendeeDTO> getEventAttendeesWithDetails(@Param("eventID") String eventID);
 
-    List<Event> findByUserUserID(String userID);
+
 
     @Query("SELECT e FROM Event e WHERE LOWER(e.eventName) LIKE LOWER(concat('%', :searchQuery, '%')) OR LOWER(e.organization) LIKE LOWER(concat('%', :searchQuery, '%'))")
     List<Event> findByEventNameContainingIgnoreCaseOrOrganizationContainingIgnoreCase(@Param("searchQuery") String searchQuery1, @Param("searchQuery") String searchQuery2);
@@ -41,11 +35,39 @@ public interface EventRepository extends JpaRepository<Event, String> {
     @Query("SELECT e FROM Event e WHERE e.startDate BETWEEN :startDate AND :endDate ORDER BY e.startDate ASC, e.eventName ASC")
     List<Event> findByStartDateBetween(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
-    
     @Query("SELECT e FROM Event e WHERE e.startDate <= :endDate AND e.endDate >= :startDate")
     List<Event> findByStartDateLessThanEqualAndEndDateGreaterThanEqual(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
 
 
+    // TOPIC, TYPE, AND RANGE DATE FILTER
+    @Query("SELECT e FROM Event e JOIN e.topics t WHERE t.name IN :topics AND e.startDate BETWEEN :startDate AND :endDate AND e.eventType = :eventType")
+    List<Event> findByTopicsNameInAndStartDateBetweenAndEventType(
+        @Param("topics") List<String> topics,
+        @Param("startDate") LocalDate startDate,
+        @Param("endDate") LocalDate endDate,
+        @Param("eventType") String eventType
+        );
+
+    // TOPIC AND RANGE DATE FILTER
+     @Query("SELECT e FROM Event e JOIN e.topics t WHERE t.name IN :topics AND e.startDate BETWEEN :startDate AND :endDate ORDER BY e.startDate ASC")
+     List<Event> findByTopicsNameInAndStartDateBetween(
+         @Param("topics") List<String> topics,
+         @Param("startDate") LocalDate startDate,
+         @Param("endDate") LocalDate endDate
+     );
+
+    //TYPE AND SINGLE DATE
+    @Query("SELECT e FROM Event e WHERE e.startDate = :date AND e.eventType = :type")
+    List<Event> findByDateAndType(@Param("date") LocalDate date, @Param("type") String type);
+
+    // RANGE DATE AND TYPE FILTER
+    @Query("SELECT e FROM Event e WHERE e.startDate BETWEEN :startDate AND :endDate AND e.eventType = :eventType")
+    List<Event> findByStartDateBetweenAndEventType(
+        @Param("startDate") LocalDate startDate,
+        @Param("endDate") LocalDate endDate,
+        @Param("eventType") String eventType
+        );
+    
 
 
     @Query("SELECT e FROM Event e JOIN e.topics t WHERE t.name IN :topics AND e.startDate = :date AND e.eventType = :eventType")
